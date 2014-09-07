25f6e400b9e2a48db03e5c147d0448a48b95e6ce
Rattan Priya <rpriya@yahoo-inc.com>
2014-09-07 13:43:37 -0700

    Initial Commit

---
+++ tipcal1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcal1
+//
+//  Created by Rattan Priya on 9/7/14.
+//  Copyright (c) 2014 Priya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcal1Tests/tipcal1Tests.m
@@ -0,0 +1,34 @@
+//
+//  tipcal1Tests.m
+//  tipcal1Tests
+//
+//  Created by Rattan Priya on 9/7/14.
+//  Copyright (c) 2014 Priya. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tipcal1Tests : XCTestCase
+
+@end
+
+@implementation tipcal1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
